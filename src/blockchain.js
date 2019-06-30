d55a0c3e1ba98a58fb9f272958b50977cc0e6814
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-06-29 19:20:27 -0500

    added API for blockchain network node

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,12 +1,17 @@
-function Blockchain () {
+const sha256 = require('sha256');
+
+function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
+
+    //Create Genesis Block
+    this.createNewBlock(1337, '0', '0');
 }
 
 //Created a new block
-Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     const newBlock = {
-        index: this.chain.length + 1,
+        blockHeight: this.chain.length + 1,
         timestamp: Date.now(),
         transactions: this.pendingTransactions,
         nonce, //Proof
@@ -24,7 +29,7 @@ Blockchain.prototype.getLastBlock = function () {
     return this.chain[this.chain.length - 1];
 }
 
-Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
     const newTransaction = {
         amount,
         sender,
@@ -33,7 +38,28 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
 
     this.pendingTransactions.push(newTransaction);
 
-    return this.getLastBlock().index + 1;
+    return this.getLastBlock().blockHeight + 1;
+}
+
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+
+    return hash;
+}
+
+// Repeatedly hash block until it finds a hash that starts with four 0s
+// Use random nonce to generate different hashes
+Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+    while (!hash.startsWith('0000')) {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+
+    return nonce;
 }
 
 module.exports = Blockchain;
--- src/test.js
+++ src/test.js
@@ -1,19 +1,44 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, 'DSKFSH437FD7HSFSDF', 'SKJFHSDKFJH9238');
+/*
+    Test hashBlock
 
-bitcoin.createNewTransaction(100, 'ALEXHJ4354543', 'JACKFHSDFH383432');
-bitcoin.createNewTransaction(10, 'ALEXHJ4354543', 'JACKFHSDFH383432');
-bitcoin.createNewTransaction(1000, 'ALEXHJ4354543', 'JACKFHSDFH383432');
+*/
+// const previousBlockHash = 'ALSKJFSLDFJSD483398U39ADF';
+// const currentBlockData = [
+//     {
+//         amount: 10,
+//         sender: 'bob',
+//         recipient: 'sally'
+//     }
+// ]
+// const nonce = 100;
+// console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce));
 
-bitcoin.createNewBlock(89766, 'JHGJHG7866', 'JKGHJHGJKG786876');
+/*
+    Test createNewBlock
+*/
+// console.log(bitcoin.createNewBlock(2389, 'DSKFSH437FD7HSFSDF', 'SKJFHSDKFJH9238'));
 
-bitcoin.createNewTransaction(100, 'ALEXHJ4354543', 'JACKFHSDFH383432');
-bitcoin.createNewTransaction(10, 'ALEXHJ4354543', 'JACKFHSDFH383432');
-bitcoin.createNewTransaction(1000, 'ALEXHJ4354543', 'JACKFHSDFH383432');
+/*
+    Test createNewTransaction
+*/
+// bitcoin.createNewBlock(2389, 'DSKFSH437FD7HSFSDF', 'SKJFHSDKFJH9238');
+// console.log(bitcoin.createNewTransaction(100, 'ALEXHJ4354543', 'JACKFHSDFH383432'));
 
-bitcoin.createNewBlock(89766, 'JHGJHG7866', 'JKGHJHGJKG786876');
+/*
+    Test proofOfWork
+*/
+// const previousBlockHash = 'ALSKJFSLDFJSD483398U39ADF';
+// const currentBlockData = [
+//     {
+//         amount: 10,
+//         sender: 'bob',
+//         recipient: 'sally'
+//     }
+// ]
+// console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,115381))
+// console.log(bitcoin.proofOfWork(previousBlockHash,currentBlockData));
 
-console.log(bitcoin.chain[2]);
+console.log(bitcoin)
